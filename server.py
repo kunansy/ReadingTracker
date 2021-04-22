a30cfbf843b80eeb7b40428181e052444f0da9f5
kunansy <kolobov.kirill@list.ru>
2021-04-22 14:39:41 +0300

    [DEV-11] template of backend added

--- server.py
+++ server.py
@@ -1,9 +1,101 @@
 #!/usr/bin/env python3
+from sanic import Sanic, Request, response
+from sanic_jinja2 import SanicJinja2
 
-def main() -> None:
+from src.tracker import Tracker, Log
+
+
+app = Sanic(__name__)
+app.static('/static', './static')
+jinja = SanicJinja2(app)
+
+log = Log()
+tracker = Tracker(log)
+
+
+@app.get('/materials/queue')
+async def get_queue(request: Request) -> response.HTTPResponse:
     pass
 
 
+@app.post('/materials/add')
+async def add_material(request: Request) -> response.HTTPResponse:
+    """ Add a material to the queue """
+    pass
+
+
+@app.post('/materials/start/<material_id:int>')
+async def start_material(request: Request,
+                         material_id: int) -> response.HTTPResponse:
+    pass
+
+
+@app.post('/materials/complete/<material_id:int>')
+async def complete_material(request: Request,
+                            materials_id: int) -> response.HTTPResponse:
+    pass
+
+
+@app.get('/materials/reading')
+async def get_reading_materials(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.get('/materials/completed')
+async def get_completed_materials(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.get('/reading_log')
+async def get_reading_log(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.post('/reading_log')
+async def add_reading_log(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.get('/notes')
+async def get_notes(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.post('/notes')
+async def add_notes(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.get('/')
+async def home(request: Request) -> response.HTTPResponse:
+    pass
+
+
+@app.exception(Exception)
+def error_handler(request: Request,
+                  exception: Exception) -> response.HTTPResponse:
+    try:
+        json = exception.json()
+    except AttributeError:
+        json = ''
+
+    context = {
+        'ok': False,
+        "wrong_request": request.json,
+        "error": {
+            "type": exception.__class__.__name__,
+            "text": str(exception),
+            "args": exception.args,
+            "json": json
+        }
+    }
+
+    return response.json(context, status=500, indent=4)
+
+
 if __name__ == "__main__":
-    main()
+    app.run(
+        port=8080,
+        debug=True,
+    )
 
