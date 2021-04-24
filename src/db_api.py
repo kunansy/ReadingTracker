6ec88770ed24ed61c0dbb2edfc3d5c9b0a23de84
kunansy <kolobov.kirill@list.ru>
2021-04-24 16:45:49 +0300

    [DEV-19] custom exceptions added to `__all__`

--- src/db_api.py
+++ src/db_api.py
@@ -1,7 +1,9 @@
 __all__ = ('get_materials', 'get_status', 'get_completed_materials',
            'get_free_materials', 'complete_material', 'get_title',
            'start_material', 'add_materials', 'get_material_status',
-           'get_reading_materials', 'add_note', 'get_notes')
+           'get_reading_materials', 'add_note', 'get_notes',
+           'BaseDBError', 'WrongDate', 'MaterialEvenCompleted',
+           'MaterialNotAssigned', 'MaterialNotFound')
 
 import datetime
 import logging
