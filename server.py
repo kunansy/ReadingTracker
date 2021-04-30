dbd3290157ff4fb785f5147ea1e7280e6bd2d7c8
kunansy <kolobov.kirill@list.ru>
2021-04-30 18:45:21 +0300

    `Tracker.statistics()` used

--- server.py
+++ server.py
@@ -179,14 +179,10 @@ async def complete_material(request: Request,
 @app.get('/materials/reading')
 @jinja.template('reading.html')
 async def get_reading_materials(request: Request) -> dict[str, Any]:
-    stat = [
-        tracker.get_material_statistic(
-            ms.material.material_id, material=ms.material, status=ms.status
-        )
-        for ms in tracker.reading
-    ]
+    statistics = tracker.statistics(tracker.reading)
+
     return {
-        'statistics': stat,
+        'statistics': statistics,
         'DATE_FORMAT': trc.DATE_FORMAT
     }
 
@@ -194,14 +190,10 @@ async def get_reading_materials(request: Request) -> dict[str, Any]:
 @app.get('/materials/completed')
 @jinja.template('completed.html')
 async def get_completed_materials(request: Request) -> dict:
-    stat = [
-        tracker.get_material_statistic(
-            ms.material.material_id, material=ms.material, status=ms.status
-        )
-        for ms in tracker.processed
-    ]
+    statistics = tracker.statistics(tracker.processed)
+
     return {
-        'statistics': stat,
+        'statistics': statistics,
         'DATE_FORMAT': trc.DATE_FORMAT
     }
 
