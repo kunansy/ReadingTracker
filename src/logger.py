45d7c6932672d5d333690d4b030755307e524ec6
kunansy <kolobov.kirill@list.ru>
2021-05-04 13:56:10 +0300

    file handlers added

--- src/logger.py
+++ src/logger.py
@@ -1,28 +1,42 @@
 import sys
 import logging
+from pathlib import Path
 
 
 MSG_FMT = "[{asctime},{msecs:3.0f}] [{name}] [{levelname:^8}] " \
           "[{module}:{funcName}] [{process}] {message}"
 DATE_FMT = "%d-%m-%Y %H:%M:%S"
+LOG_FOLDER = Path('./logs')
+LOG_FILE = LOG_FOLDER / 'tracker.log'
+
+LOG_FOLDER.mkdir(exist_ok=True)
 
 formatter = logging.Formatter(
-    fmt=MSG_FMT, datefmt=DATE_FMT, style='{')
+    fmt=MSG_FMT, datefmt=DATE_FMT, style='{'
+)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
 
+file_handler = logging.FileHandler(
+    LOG_FILE, delay=True
+)
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(formatter)
+
 logger = logging.getLogger('ReadingTracker')
 logger.setLevel(logging.DEBUG)
 
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 
 # ---- config for sanic log ----
 BASE_MESSAGE_FORMAT = "[%(asctime)s] [%(name)s] [%(levelname)s] " \
                       "[%(module)s:%(funcName)s():%(process)d]"
 DATE_FORMAT = "%d-%m-%Y %H:%M:%S"
+SANIC_LOG_FILE = LOG_FOLDER / 'sanic.log'
 
 LOGGING_CONFIG = {
     "version": 1,
@@ -45,6 +59,12 @@ LOGGING_CONFIG = {
             "stream": sys.stderr,
             "level": "DEBUG"
         },
+        "internalFile": {
+            "class": "logging.FileHandler",
+            "filename": SANIC_LOG_FILE,
+            "formatter": "simple",
+            "level": "WARNING"
+        },
         "accessStream": {
             "class": "logging.StreamHandler",
             "formatter": "access",
@@ -54,11 +74,15 @@ LOGGING_CONFIG = {
     "loggers": {
         "sanic.root": {
             "level": "DEBUG",
-            "handlers": ["internalStream"]
+            "handlers": ["internalStream", "internalFile"]
         },
         "sanic.access": {
             "level": "DEBUG",
             "handlers": ["accessStream"]
+        },
+        "sanic.error": {
+            "level": "WARNING",
+            "handlers": ["internalFile"]
         }
     }
 }
