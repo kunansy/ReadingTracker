1dd4a7fe242783dd7758e14613e5a54fa33bfeff
kunansy <kolobov.kirill@list.ru>
2021-10-10 19:07:48 +0300

    DB_URI added

--- tracker/common/settings.py
+++ tracker/common/settings.py
@@ -9,13 +9,23 @@ env.read_env()
 
 DATE_FORMAT = '%d-%m-%Y'
 DATA_FOLDER = Path('data')
+DSN_TEMPLATE = "postgresql+asyncpg://{username}:{password}" \
+               "@{host}:{port}/{db_name}"
 
 with env.prefixed("DB_"):
     DB_HOST = env("HOST")
     DB_PORT = env.int("PORT")
     DB_NAME = env("NAME")
-    DB_USER = env("USER")
+    DB_USERNAME = env("USERNAME")
     DB_PASSWORD = env("PASSWORD")
+
+DB_URI = DSN_TEMPLATE.format(
+    username=DB_USERNAME,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
+    db_name=DB_NAME
+)
 
 with env.prefixed("LOGGER_"):
     LOGGER_NAME = env("NAME")
