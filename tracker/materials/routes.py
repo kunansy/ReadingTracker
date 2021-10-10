9108e9a54e7fd8250ce899ce577177cdc1d8f64c
kunansy <kolobov.kirill@list.ru>
2021-10-10 21:14:55 +0300

    /materials in process

--- tracker/materials/routes.py
+++ tracker/materials/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 from tracker.common import settings
 from tracker.materials import db, schemas
@@ -9,21 +10,26 @@ router = APIRouter(
     prefix="/materials",
     tags=["materials"]
 )
+templates = Jinja2Templates(directory="templates")
 
 
 @router.get('/queue', response_class=HTMLResponse)
 async def get_queue(request: Request):
-    return {
+    estimates = await db.estimate()
+
+    context = {
         'request': request,
-        'estimates': tracker.estimate(),
+        'estimates': estimates,
         'DATE_FORMAT': settings.DATE_FORMAT
     }
+
+    return templates.TemplateResponse("queue.html", context)
 
 
 @router.get('/add', response_class=HTMLResponse)
 async def add_material_view(request: Request):
     """ Add a material to the queue """
-    return {
+    context = {
         'request': request,
         'title': request.ctx.session.get('title', ''),
         'authors': request.ctx.session.get('authors', ''),
@@ -31,62 +37,53 @@ async def add_material_view(request: Request):
         'tags': request.ctx.session.get('tags', ''),
     }
 
+    return templates.TemplateResponse("add_material.html", context)
+
 
 @router.post('/add', response_class=HTMLResponse)
-async def add_material(request: Request,
-                       material: schemas.Material):
+async def add_material(material: schemas.Material):
     """ Add a material to the queue """
-    form_items = await get_form_items(request)
+    await db.add_material(**material.dict())
+    # jinja.flash(request, "Material added", 'success')
 
-    try:
-        material = validators.Material(**form_items)
-    except ValidationError as e:
-        context = ujson.dumps(e.errors(), indent=4)
-        sanic_logger.warning(f"Validation error:\n{context}")
-
-        for error in e.errors():
-            jinja.flash(request, f"{error['loc'][0]}: {error['msg']}", 'error')
-
-        request.ctx.session.update(**form_items)
-        return response.redirect('/materials/add')
-    else:
-        request.ctx.session.clear()
-
-    tracker.add_material(**material.dict())
-    jinja.flash(request, "Material added", 'success')
-
-    return response.redirect('/materials/add')
+    return RedirectResponse('/materials/add')
 
 
 @router.post('/start/{material_id}')
 async def start_material(material_id: int):
-    tracker.start_material(material_id)
-    jinja.flash(request, f"Material {material_id=} started", 'success')
+    await db.start_material(material_id=material_id)
+    # jinja.flash(request, f"Material {material_id=} started", 'success')
 
-    return response.redirect('/materials/queue')
+    return RedirectResponse('/materials/queue')
 
 
 @router.post('/complete/{material_id}')
 async def complete_material(material_id: int):
-    tracker.complete_material(material_id)
-    jinja.flash(request, f"Material {material_id=} completed", 'success')
+    await db.complete_material(material_id)
+    # jinja.flash(request, f"Material {material_id=} completed", 'success')
 
-    return response.redirect('/materials/reading')
+    return RedirectResponse('/materials/reading')
 
 
 @router.get('/reading', response_class=HTMLResponse)
 async def get_reading_materials(request: Request):
-    return {
+    statistics = await db.reading_statistics()
+
+    context = {
         'request': request,
-        'statistics': tracker.statistics(tracker.reading),
+        'statistics': statistics,
         'DATE_FORMAT': settings.DATE_FORMAT
     }
+    return templates.TemplateResponse("reading.html", context)
 
 
 @router.get('/completed', response_class=HTMLResponse)
 async def get_completed_materials(request: Request):
-    return {
+    statistics = await db.processed_statistics()
+
+    context = {
         'request': request,
-        'statistics': tracker.statistics(tracker.processed),
+        'statistics': statistics,
         'DATE_FORMAT': settings.DATE_FORMAT
     }
+    return templates.TemplateResponse("completed.html", context)
