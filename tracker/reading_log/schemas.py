63f44b0d597d383e33455d7e06d954485044cfb3
kunansy <kolobov.kirill@list.ru>
2021-10-16 21:17:41 +0300

    LogRecord schema updated
    some validations removed, minor refactoring

--- tracker/reading_log/schemas.py
+++ tracker/reading_log/schemas.py
@@ -1,24 +1,15 @@
 import datetime
 
-from pydantic import BaseModel, conint, validator
-
-from tracker.common import database
+from pydantic import BaseModel, PositiveInt, validator
 
 
 class LogRecord(BaseModel):
-    material_id: conint(gt=0) # type: ignore
+    material_id: PositiveInt # type: ignore
     date: datetime.date # type: ignore
-    count: conint(gt=0) # type: ignore
+    count: PositiveInt # type: ignore
 
     @validator('date')
     def validate_date(cls,
                       date: datetime.date) -> datetime.date:
-        assert date <= datetime.datetime.utcnow(), "You cannot set log to the future"
+        assert date <= datetime.datetime.utcnow().date(), "You cannot set log to the future"
         return date
-
-    @validator('material_id')
-    def validate_material_reading(cls,
-                                  material_id: int) -> int:
-        assert database.is_material_reading(material_id), \
-            f"Material {material_id=} is not reading"
-        return material_id
