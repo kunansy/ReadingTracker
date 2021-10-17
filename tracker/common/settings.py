cb821c4493b8bc9bc0595f5ab3fffbdd8bcabafe
kunansy <kolobov.kirill@list.ru>
2021-10-17 19:00:39 +0300

    setting names fixed

--- tracker/common/settings.py
+++ tracker/common/settings.py
@@ -12,8 +12,8 @@ DSN_TEMPLATE = "postgresql+asyncpg://{username}:{password}" \
 
 with env.prefixed("API_"):
     API_PORT = env.int("PORT")
-    API_DEBUG = env.bool("API_DEBUG", False)
-    API_VERSION = env("API_VERSION")
+    API_DEBUG = env.bool("DEBUG", False)
+    API_VERSION = env("VERSION")
 
 with env.prefixed("DB_"):
     DB_HOST = env("HOST")
