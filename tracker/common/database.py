400e67f8ecef2f2435adf6f4ea40613704fc11b0
kunansy <kolobov.kirill@list.ru>
2021-10-19 09:17:57 +0300

    named tuples removed

--- tracker/common/database.py
+++ tracker/common/database.py
@@ -1,6 +1,6 @@
 import datetime
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, NamedTuple
+from typing import AsyncGenerator
 from uuid import UUID
 
 import sqlalchemy.sql as sa
@@ -12,16 +12,6 @@ from tracker.common.log import logger
 
 class DatabaseError(Exception):
     pass
-
-
-class CompletionDate(NamedTuple):
-    material_id: UUID
-    completed_at: datetime.date
-
-
-class MaterialTitle(NamedTuple):
-    material_id: UUID
-    title: str
 
 
 engine = create_async_engine(settings.DB_URI, encoding='utf-8')
@@ -50,7 +40,7 @@ def yesterday() -> datetime.datetime:
     return today() - datetime.timedelta(days=1)
 
 
-async def get_completion_dates() -> list[CompletionDate]:
+async def get_completion_dates() -> dict[UUID, datetime.date]:
     logger.debug("Getting completion dates")
 
     stmt = sa.select([models.Materials.c.material_id,
@@ -60,20 +50,20 @@ async def get_completion_dates() -> list[CompletionDate]:
         .where(models.Statuses.completed_at != None)
 
     async with session() as ses:
-        return [
-            CompletionDate(row.material_id, row.completed_at)
+        return {
+            row.material_id: row.completed_at
             async for row in await ses.stream(stmt)
-        ]
+        }
 
 
-async def get_material_titles() -> list[MaterialTitle]:
+async def get_material_titles() -> dict[UUID, str]:
     logger.debug("Getting material titles")
 
     stmt = sa.select([models.Materials.c.material_id,
                       models.Materials.c.title])
 
     async with session() as ses:
-        return [
-            MaterialTitle(row.material_id, row.title)
+        return {
+            row.material_id: row.title
             async for row in await ses.stream(stmt)
-        ]
+        }
