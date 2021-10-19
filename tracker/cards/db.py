aab8ad988fce799f747a3e39fcffe5d97e2258b3
kunansy <kolobov.kirill@list.ru>
2021-10-19 09:21:08 +0300

    add_card() updated
    use models and sa.core instead of sa.orm, recall usage removed

--- tracker/cards/db.py
+++ tracker/cards/db.py
@@ -26,59 +26,23 @@ async def notes_with_cards(*,
 
 
 async def add_card(*,
-                   material_id: int,
+                   material_id: UUID,
+                   note_id: UUID,
                    question: str,
-                   note_id: int,
                    answer: Optional[str] = None) -> None:
     logger.debug("Adding new card")
-    today_ = database.today()
 
+    values = {
+        "material_id": material_id,
+        "note_id": note_id,
+        "question": question,
+        "answer": answer,
+    }
+    stmt = models.Cards\
+        .insert().values(values)
     async with database.session() as ses:
-        card = Card(
-            material_id=material_id,
-            question=question,
-            answer=answer,
-            note_id=note_id,
-            date=today_
-        )
-        ses.add(card)
-
-        # commit required to get card_id
-        ses.commit()
-        logger.info("Card added")
-
-        logger.debug("Starting the card")
-        recall = Recall(
-            card_id=card.card_id,
-            last_repeat_date=today_,
-            next_repeat_date=today_
-        )
-        ses.add(recall)
-        logger.debug("Card started")
-
-
-async def get_cards(*,
-                    material_ids: Optional[list[int]] = None) -> list[CardNoteRecall]:
-    how_many = 'all materials'
-    if material_ids:
-        how_many = f"material {material_ids=}"
-
-    logger.info(f"Getting cards for {how_many}")
-
-    async with database.session() as ses:
-        query = ses.query(Card, Recall, Note) \
-            .join(Recall, Card.card_id == Recall.card_id) \
-            .join(Note, Card.note_id == Note.id) \
-            .filter(Recall.next_repeat_date <= today())
-
-        if material_ids:
-            query = query \
-                .filter(Card.material_id.in_(material_ids))
-
-    return [
-        CardNoteRecall(card=card, note=note, recall=recall)
-        for card, recall, note in query.all()
-    ]
+        await ses.execute(stmt)
+    logger.debug("Card added")
 
 
 async def all_cards(*,
