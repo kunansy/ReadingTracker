ec5787d9e7444a7949cec16aed002089bbacb292
kunansy <kolobov.kirill@list.ru>
2021-10-20 19:40:12 +0300

    some log methods moved

--- tracker/reading_log/db.py
+++ tracker/reading_log/db.py
@@ -92,6 +92,118 @@ async def data() -> AsyncGenerator[tuple[datetime.date, LogRecord], None]:
         iter_over_dates += step
 
 
+async def get_start_date() -> datetime.date:
+    stmt = sa.select(sa.func.min(models.ReadingLog.c.date))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_stop_date() -> datetime.date:
+    stmt = sa.select(sa.func.max(models.ReadingLog.c.date))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_log_duration() -> int:
+    stmt = sa.select(sa.func.max(models.ReadingLog.c.date) -
+                     sa.func.min(models.ReadingLog.c.date))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_total_read_pages() -> int:
+    stmt = sa.select(sa.func.sum(models.ReadingLog.count))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_lost_days() -> int:
+    stmt = sa.select(sa.func.max(models.ReadingLog.c.date) -
+                     sa.func.min(models.ReadingLog.c.date) -
+                     sa.func.count(1))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_average_pages_read_per_day() -> int:
+    stmt = sa.select(sa.func.avg(models.ReadingLog.count))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_median_pages_read_per_day() -> int:
+    stmt = sa.select(sa.func.median(models.ReadingLog.count))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def contains(*,
+                   material_id: UUID) -> bool:
+    stmt = sa.select(sa.func.count(1) >= 1)\
+        .select_from(models.ReadingLog)\
+        .where(models.ReadingLog.c.material_id == str(material_id))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_min_record() -> Optional[RowMapping]:
+    stmt = sa.select([models.ReadingLog,
+                      models.Materials.c.title])\
+        .join(models.Materials,
+              models.ReadingLog.c.material_id == models.Materials.c.material_id) \
+        .order_by(models.ReadingLog.c.count)\
+        .limit(1)
+
+    async with database.session() as ses:
+        return (await ses.execute(stmt)).first()
+
+
+async def get_max_record() -> Optional[RowMapping]:
+    stmt = sa.select([models.ReadingLog,
+                      models.Materials.c.title]) \
+        .join(models.Materials,
+              models.ReadingLog.c.material_id == models.Materials.c.material_id) \
+        .order_by(models.ReadingLog.c.count.desc()) \
+        .limit(1)
+
+    async with database.session() as ses:
+        return (await ses.execute(stmt)).first()
+
+
+async def would_be_total() -> int:
+    stmt = sa.select(sa.func.sum(models.ReadingLog.c.count) +
+                     sa.func.avg(models.ReadingLog.c.count) * (
+                     sa.func.max(models.ReadingLog.c.date) -
+                     sa.func.min(models.ReadingLog.c.date) -
+                     sa.func.count(1)))
+
+    async with database.session() as ses:
+        return await ses.scalar(stmt)
+
+
+async def get_log_statistics():
+    return {
+        "start_date": await get_start_date(),
+        "stop_date": await get_stop_date(),
+        "duration": await get_log_duration(),
+        "lost_time": await get_lost_days(),
+        "average": await get_average_pages_read_per_day(),
+        "total_pages_read": await get_total_read_pages(),
+        "would_be_total": await would_be_total(),
+        "min": await get_min_record(),
+        "max": await get_max_record(),
+        "median": await get_median_pages_read_per_day()
+    }
+
+
 async def get_material_reading_now() -> Optional[int]:
     if not await get_log_records():
         logger.warning("Reading log is empty, no materials reading")
