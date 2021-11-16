48fde32f31894823ebe6c96dbbeea438ee949be1
kunansy <kolobov.kirill@list.ru>
2021-11-16 19:43:16 +0300

    rendering templates for errors added

--- tracker/main.py
+++ tracker/main.py
@@ -1,12 +1,12 @@
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import UJSONResponse
+from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+from tracker.cards.routes import router as cards_router
 from tracker.common import database, settings
 from tracker.common.log import logger
-from tracker.cards.routes import router as cards_router
 # from tracker.materials.routes import router as materials_router
 from tracker.notes.routes import router as notes_router
 from tracker.reading_log.routes import router as reading_log_router
@@ -22,6 +22,7 @@ app = FastAPI(
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(reading_log_router)
 app.include_router(notes_router)
@@ -29,25 +30,51 @@ app.include_router(notes_router)
 app.include_router(cards_router)
 
 
-# TODO: render error template
 @app.exception_handler(database.DatabaseError)
 async def database_exception_handler(request: Request,
                                      exc: database.DatabaseError):
-    logger.exception("Error with the database, %s", str(exc))
-    return UJSONResponse(
-        status_code=500,
-        content={"message": "Internal server error"},
-    )
+    logger.exception("Error, %s", str(exc))
+
+    context = {
+        "request": request,
+        "error": {
+            "type": exc.__class__.__name__,
+            "args": exc.args,
+            "json": f"Database error: {exc}"
+        }
+    }
+
+    return templates.TemplateResponse("errors/500.html", context)
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request,
                                        exc: RequestValidationError):
     logger.exception("Validation error occurred, %s", str(exc))
-    return UJSONResponse(
-        status_code=422,
-        content={"message": "Validation error"},
-    )
+
+    context = {
+        "request": request,
+        "error": {
+            "type": exc.__class__.__name__,
+            "args": exc.args,
+            "json": exc.json(indent=4)
+        }
+    }
+
+    return templates.TemplateResponse("errors/500.html", context)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request,
+                                 exc: HTTPException):
+    logger.exception("HTTP exception occurred, %s", str(exc))
+
+    context = {
+        "request": request,
+        "what": repr(exc)
+    }
+
+    return templates.TemplateResponse("errors/404.html", context)
 
 
 if __name__ == '__main__':
