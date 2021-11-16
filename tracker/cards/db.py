a0fbcfc6977bb360edae60ed1967187c47a1f655
kunansy <kolobov.kirill@list.ru>
2021-11-16 18:42:39 +0300

    db updated

--- tracker/cards/db.py
+++ tracker/cards/db.py
@@ -8,82 +8,67 @@ from tracker.common import database, models
 from tracker.common.log import logger
 
 
-async def notes_with_cards(*,
-                           material_ids: Optional[list[UUID]] = None) -> set[UUID]:
-    how_many = 'all'
-    if material_ids:
-        how_many = str(len(material_ids))
-    logger.info(f"Getting notes with a card for {how_many} materials")
+async def notes_with_cards() -> list[UUID]:
+    logger.info("Getting notes with a card")
 
     stmt = sa.select(models.Notes.c.note_id)\
         .join(models.Cards,
-              models.Cards.c.note_id == models.Cards.c.note_id)
-    if material_ids:
-        stmt = stmt \
-            .where(models.Notes.c.material_id.in_(material_ids))
+              models.Cards.c.note_id == models.Notes.c.note_id)
 
     async with database.session() as ses:
         return (await ses.execute(stmt)).all()
 
 
-async def get_material_titles() -> dict[UUID, str]:
-    return await database.get_material_titles()
-
-
 async def add_card(*,
-                   card: schemas.Card) -> None:
+                   material_id: UUID,
+                   note_id: UUID,
+                   question: str,
+                   answer: Optional[str] = None) -> None:
     logger.debug("Adding new card")
 
     values = {
-        "material_id": str(card.material_id),
-        "note_id": str(card.note_id),
-        "question": card.question,
-        "answer": card.answer,
+        "material_id": str(material_id),
+        "note_id": str(note_id),
+        "question": question,
+        "answer": answer,
     }
     stmt = models.Cards\
         .insert().values(values)
 
     async with database.session() as ses:
         await ses.execute(stmt)
+
     logger.debug("Card added")
 
 
-async def get_cards_list(*,
-                         material_ids: Optional[list[UUID]] = None) -> list[dict[str, Any]]:
-    how_many = 'all materials'
-    if material_ids:
-        how_many = f"material {material_ids=}"
+async def get_cards_list() -> list[dict[str, Any]]:
+    logger.info("Getting all cards")
 
-    logger.info(f"Getting all cards for {how_many}")
-
-    stmt = sa.select([models.Cards, models.Notes]) \
+    stmt = sa.select([models.Cards,
+                      models.Notes,
+                      models.Materials.c.title]) \
         .join(models.Notes,
-              models.Cards.c.note_id == models.Notes.c.id)
-
-    if material_ids:
-        stmt = stmt \
-            .where(models.Cards.c.material_id.in_(str(_id) for _id in material_ids))
+              models.Cards.c.note_id == models.Notes.c.note_id)\
+        .join(models.Materials,
+              models.Notes.c.material_id == models.Materials.c.material_id)
 
     async with database.session() as ses:
         return [
-            {"card": card, "note": note}
-            async for card, note in await ses.stream(stmt)
+            {
+                "card": {"card_id": row.card_id, "question": row.question,
+                         "answer": row.answer, "added_at": row.added_at},
+                "note": {"note_id": row.note_id, "material_title": row.title,
+                         "content": row.content, "page": row.page, "chapter": row.chapter}
+            }
+            async for row in await ses.stream(stmt)
         ]
 
 
-async def get_cards_count(*,
-                          material_ids: Optional[list[UUID]] = None) -> int:
-    how_many = 'all materials'
-    if material_ids:
-        how_many = f"material {material_ids=}"
-
-    logger.debug(f"Getting amount of cards for {how_many}")
+async def get_cards_count() -> int:
+    logger.debug("Getting amount of cards")
 
     stmt = sa.select(sa.func.count(1))\
         .select_from(models.Cards)
-    if material_ids:
-        stmt = stmt\
-            .where(models.Cards.c.material_id.in_(str(_id) for _id in material_ids))
 
     async with database.session() as ses:
         return await ses.scalar(stmt)
