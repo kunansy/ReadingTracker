37802f6a28668bb89ee017167b139a4f3eb50d14
kunansy <kolobov.kirill@list.ru>
2022-04-11 10:40:22 +0300

    DEV-#46: get/update note implemented

--- tracker/notes/db.py
+++ tracker/notes/db.py
@@ -55,3 +55,36 @@ async def add_note(*,
         note_id = (await ses.execute(stmt)).one()[0]
 
     logger.debug("Note_id=%s added", note_id)
+
+
+async def get_note(*,
+                   note_id: UUID) -> RowMapping | None:
+    logger.debug("Getting note_id=%s", note_id)
+
+    stmt = sa.select(models.Notes) \
+        .where(models.Notes.c.note_id == str(note_id))
+
+    async with database.session() as ses:
+        return (await ses.execute(stmt)).mappings().one_or_none()
+
+
+async def update_note(*,
+                      note_id: UUID,
+                      content: str,
+                      page: int,
+                      chapter: int) -> None:
+    logger.debug("Updating note_id=%s", note_id)
+
+    values = {
+        'content': content,
+        'page': page,
+        'chapter': chapter
+    }
+    stmt = models.Notes. \
+        update().values(values) \
+        .where(models.Notes.c.note_id == str(note_id))
+
+    async with database.session() as ses:
+        await ses.execute(stmt)
+
+    logger.debug("Note_id=%s updated", note_id)
