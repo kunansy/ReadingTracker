e40a67e52f3bf7b409aaa4c82d885383b420e53a
kunansy <kolobov.kirill@list.ru>
2022-04-19 11:05:22 +0300

    _replace_quotes() added

--- tracker/notes/schemas.py
+++ tracker/notes/schemas.py
@@ -4,6 +4,13 @@ from fastapi import Form
 from pydantic import BaseModel, conint, constr, validator
 
 
+def _replace_quotes(note: str) -> str:
+    while '"' in note:
+        note = note.replace('"', "«", 1)
+        note = note.replace('"', "»", 1)
+    return note
+
+
 class Note(BaseModel):
     material_id: UUID
     content: constr(strip_whitespace=True)
@@ -27,6 +34,7 @@ class Note(BaseModel):
 
         # always use uppercase for first letter
         content = f"{content[0].upper()}{content[1:]}"
+        content = _replace_quotes(content)
 
         return content\
             .replace('--', "–")\
