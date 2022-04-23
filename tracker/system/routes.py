230bd9656913acd8e7e1b8ef24dee043e9a57fb7
kunansy <kolobov.kirill@list.ru>
2022-04-23 17:47:06 +0300

    dev-#34: /system/graphic implemented

--- tracker/system/routes.py
+++ tracker/system/routes.py
@@ -1,9 +1,13 @@
+from uuid import UUID
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from tracker.common.log import logger
 from tracker.google_drive import drive_api
+from tracker.reading_log import db as reading_log_db
+from tracker.system import db
 
 
 router = APIRouter(
@@ -14,13 +18,36 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get('/')
-async def system_view(request: Request):
+@router.get('/',
+            response_class=RedirectResponse)
+async def system_view():
+    redirect_path = router.url_path_for(graphic.__name__)
+    material_id = await reading_log_db.get_material_reading_now()
+
+    redirect_url = f"{redirect_path}?material_id={material_id}"
+    return RedirectResponse(redirect_url, status_code=302)
+
+
+@router.get('/graphic',
+            response_class=HTMLResponse)
+async def graphic(request: Request,
+                  material_id: UUID,
+                  last_days: int = 7):
+    graphic_image = await db.create_reading_graphic(
+        material_id=material_id,
+        last_days=last_days
+    )
+    titles = await db.get_read_material_titles()
+
     context = {
         'request': request,
-        'is_ok': request.cookies.get('status')
+        'material_id': material_id,
+        'last_days': last_days,
+        'graphic_image': graphic_image,
+        'titles': titles
     }
-    return templates.TemplateResponse("system.html", context)
+
+    return templates.TemplateResponse("graphic.html", context)
 
 
 @router.get('/backup',
