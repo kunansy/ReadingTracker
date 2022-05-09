42203c92d2900799f567a0e246bad5d20bfe7dfa
kunansy <kolobov.kirill@list.ru>
2022-05-09 16:41:09 +0300

    whoops, quickfix
    order of punctuation mapping fixed

--- tracker/notes/schemas.py
+++ tracker/notes/schemas.py
@@ -7,9 +7,9 @@ from pydantic import BaseModel, conint, constr, validator
 
 PUNCTUATION_MAPPING = {
     "--": "—",
+    "<->": "↔",
     "->": "→",
     "<-": "←",
-    "<->": "↔",
 }
 BOLD_MARKER = "font-weight-bold"
 ITALIC_MARKER = "font-italic"
