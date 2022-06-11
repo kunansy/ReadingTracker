2e526b70d318f8499d12f7c2964daec7b9b57411
kunansy <kolobov.kirill@list.ru>
2022-06-11 15:51:02 +0300

    DEV-#49: cache() decorator implemented

--- tracker/common/database.py
+++ tracker/common/database.py
@@ -1,6 +1,7 @@
 import datetime
+import functools
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, NamedTuple
+from typing import AsyncGenerator, NamedTuple, Any
 from uuid import UUID
 
 import sqlalchemy.sql as sa
@@ -27,6 +28,50 @@ engine = create_async_engine(
     encoding='utf-8',
     isolation_level='REPEATABLE READ'
 )
+
+utcnow = datetime.datetime.utcnow
+
+
+class TTLCache:
+    # TTL is 20s
+    TTL = 20
+
+    def __init__(self, result: list[dict[str, Any]]):
+        self.added_at = utcnow()
+        self.result = result
+
+    def is_alive(self) -> bool:
+        return (utcnow() - self.added_at).seconds <= self.TTL
+
+
+def cache(func):
+    storage: dict[int, TTLCache] = {}
+    name = func.__name__
+
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs):
+        nonlocal storage
+
+        hashable_kwargs = {
+            k: tuple(v) if isinstance(v, (list, set, dict)) else v
+            for k, v in kwargs.items()
+        }
+        arg = hash((*args, *hashable_kwargs.items()))
+        if not (ttl := storage.get(arg)):
+            logger.log(5, "%s: got from func", name)
+
+            result = await func(*args, **kwargs)
+            storage[arg] = TTLCache(result)
+        elif not ttl.is_alive():
+            logger.log(5, "%s: cache expired, get the new one", name)
+
+            result = await func(*args, **kwargs)
+            storage[arg] = TTLCache(result)
+        else:
+            logger.log(5, "%s: got from cache", name)
+
+        return storage[arg].result
+    return wrapped
 
 
 @asynccontextmanager
