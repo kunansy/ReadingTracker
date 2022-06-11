9a47ef9f8d5cc110885b8baef681e872a4ad7293
kunansy <kolobov.kirill@list.ru>
2022-06-11 19:46:43 +0300

    DEV-#49: logs improved

--- tracker/notes/db.py
+++ tracker/notes/db.py
@@ -57,7 +57,7 @@ async def get_material_with_notes_titles() -> dict[str, str]:
 
 
 async def get_notes() -> list[Note]:
-    logger.debug("Getting notes")
+    logger.debug("Getting all notes")
     stmt = sa.select(models.Notes)
 
     async with database.session() as ses:
@@ -69,7 +69,7 @@ async def get_notes() -> list[Note]:
 
 async def get_material_notes(*,
                              material_id: UUID) -> list[Note]:
-    logger.debug("Getting material_id=%s notes", material_id)
+    logger.debug("Getting material_id='%s' notes", material_id)
 
     stmt = sa.select(models.Notes) \
         .where(models.Notes.c.material_id == str(material_id))
@@ -83,15 +83,18 @@ async def get_material_notes(*,
 
 async def get_note(*,
                    note_id: UUID) -> Note | None:
-    logger.debug("Getting note_id=%s", note_id)
+    logger.debug("Getting note_id='%s'", note_id)
 
     stmt = sa.select(models.Notes) \
         .where(models.Notes.c.note_id == str(note_id))
 
     async with database.session() as ses:
         if note := (await ses.execute(stmt)).mappings().one_or_none():
+            logger.debug("Note_id='%s' got", note_id)
             return Note(**note)
-        return None
+
+    logger.debug("Note_id='%s' not found", note_id)
+    return None
 
 
 async def get_notes_count(*,
@@ -105,6 +108,8 @@ async def get_notes_count(*,
 
 
 async def get_all_notes_count() -> dict[UUID, int]:
+    logger.debug("Getting all notes count")
+
     stmt = sa.select([models.Notes.c.material_id.label('material_id'),
                       sa.func.count(1).label('count')]) \
         .select_from(models.Notes) \
@@ -124,8 +129,7 @@ async def add_note(*,
                    page: int,
                    date: datetime.date | None = None) -> UUID:
     date = date or database.today()
-    logger.debug("Adding note for material_id=%s at %s",
-                 material_id, date)
+    logger.debug("Adding note for material_id='%s'", material_id)
 
     values = {
         'material_id': str(material_id),
@@ -141,7 +145,7 @@ async def add_note(*,
     async with database.session() as ses:
         note_id = (await ses.execute(stmt)).one()[0]
 
-    logger.debug("Note_id=%s added", note_id)
+    logger.debug("Note_id='%s' added", note_id)
     return note_id
 
 
@@ -150,7 +154,7 @@ async def update_note(*,
                       content: str,
                       page: int,
                       chapter: int) -> None:
-    logger.debug("Updating note_id=%s", note_id)
+    logger.debug("Updating note_id='%s'", note_id)
 
     values = {
         'content': content,
@@ -164,4 +168,4 @@ async def update_note(*,
     async with database.session() as ses:
         await ses.execute(stmt)
 
-    logger.debug("Note_id=%s updated", note_id)
+    logger.debug("Note_id='%s' updated", note_id)
