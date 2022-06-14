810dd338c83368f899db48b545e4f17c775a65a0
kunansy <kolobov.kirill@list.ru>
2022-06-14 08:09:39 +0300

    DEV-#51: link added to Material schema

--- tracker/materials/schemas.py
+++ tracker/materials/schemas.py
@@ -1,5 +1,5 @@
 from fastapi import Form
-from pydantic import BaseModel, conint
+from pydantic import BaseModel, conint, HttpUrl
 
 
 class Material(BaseModel):
@@ -7,15 +7,18 @@ class Material(BaseModel):
     authors: str
     pages: conint(ge=1)
     tags: str | None = None
+    link: HttpUrl | None = None
 
     def __init__(self,
                  title: str = Form(...),
                  authors: str = Form(...),
                  pages: int = Form(...),
-                 tags: str | None = Form(None)) -> None:
+                 tags: str | None = Form(None),
+                 link: HttpUrl | None = Form(None)) -> None:
         super().__init__(
             title=title,
             authors=authors,
             pages=pages,
             tags=tags,
+            link=link
         )
