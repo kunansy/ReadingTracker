43131c9e4f9f5e2755c8256ae2407d7678b39b9b
kunansy <kolobov.kirill@list.ru>
2022-06-15 16:14:36 +0300

    DEV-#53: material_type added to Material schema

--- tracker/materials/schemas.py
+++ tracker/materials/schemas.py
@@ -1,11 +1,14 @@
 from fastapi import Form
 from pydantic import BaseModel, conint, HttpUrl
 
+from tracker.models import enums
+
 
 class Material(BaseModel):
     title: str
     authors: str
     pages: conint(ge=1)
+    material_type: enums.MaterialTypesEnum
     tags: str | None = None
     link: HttpUrl | None = None
 
@@ -13,12 +16,14 @@ class Material(BaseModel):
                  title: str = Form(...),
                  authors: str = Form(...),
                  pages: int = Form(...),
+                 material_type: enums.MaterialTypesEnum = Form(...),
                  tags: str | None = Form(None),
                  link: HttpUrl | None = Form(None)) -> None:
         super().__init__(
             title=title,
             authors=authors,
             pages=pages,
+            material_type=material_type,
             tags=tags,
             link=link
         )
