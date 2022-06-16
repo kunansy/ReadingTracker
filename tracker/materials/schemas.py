7e09acc60a0079bdec7df5a959eb601514791cee
kunansy <kolobov.kirill@list.ru>
2022-06-16 12:38:53 +0300

    DEV-#55: UpdateMaterial schema implemented

--- tracker/materials/schemas.py
+++ tracker/materials/schemas.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from fastapi import Form
 from pydantic import BaseModel, conint, HttpUrl
 
@@ -18,12 +20,36 @@ class Material(BaseModel):
                  pages: int = Form(...),
                  material_type: enums.MaterialTypesEnum = Form(...),
                  tags: str | None = Form(None),
-                 link: HttpUrl | None = Form(None)) -> None:
+                 link: HttpUrl | None = Form(None),
+                 **kwargs) -> None:
         super().__init__(
             title=title,
             authors=authors,
             pages=pages,
             material_type=material_type,
             tags=tags,
-            link=link
+            link=link,
+            **kwargs
+        )
+
+
+class UpdateMaterial(Material):
+    material_id: UUID
+
+    def __init__(self,
+                 material_id: UUID = Form(...),
+                 title: str = Form(...),
+                 authors: str = Form(...),
+                 pages: int = Form(...),
+                 material_type: enums.MaterialTypesEnum = Form(...),
+                 tags: str | None = Form(None),
+                 link: HttpUrl | None = Form(None)) -> None:
+        super().__init__(
+            material_id=material_id,
+            title=title,
+            authors=authors,
+            pages=pages,
+            material_type=material_type,
+            tags=tags,
+            link=link,
         )
