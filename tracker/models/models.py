c0eb3ca1e3da398d9b653550f21cffa09fc44920
kunansy <kolobov.kirill@list.ru>
2022-07-04 12:53:07 +0300

    DEV-#68: reading_log unique constraint added

--- tracker/models/models.py
+++ tracker/models/models.py
@@ -64,7 +64,9 @@ ReadingLog = Table(
     PrimaryKey('log_id'),
     Column('material_id', ForeignKey('materials.material_id'), index=True),
     Column('count', Integer),
-    Column('date', Date, default=_utc_now, unique=True)
+    Column('date', Date, default=_utc_now, unique=True),
+
+    UniqueConstraint('material_id', 'date', name='uix_reading_log')
 )
 
 Statuses = Table(
