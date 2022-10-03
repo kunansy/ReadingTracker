aacf51e9638d13c72990eb0b6302f68c68b4111e
kunansy <kolobov.kirill@list.ru>
2022-10-03 08:57:28 +0300

    CustomBaseModel added

---
+++ tracker/common/schemas.py
@@ -0,0 +1,15 @@
+from typing import Any, Callable
+
+import orjson as orjson
+from pydantic import BaseModel
+
+
+def orjson_dumps(v: Any, *, default: Callable[[Any], Any] | None = None) -> str:
+    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+    return orjson.dumps(v, default=default).decode()
+
+
+class CustomBaseModel(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
