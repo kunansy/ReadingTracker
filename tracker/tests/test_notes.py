f3009c0ff1159f3a29368a3aa3f57b3cd1b11e22
kunansy <kolobov.kirill@list.ru>
2023-01-18 13:41:22 +0300

    tests moved

--- tracker/tests/test_notes.py
+++ tracker/tests/test_notes.py
@@ -9,49 +9,6 @@ from tracker.notes import db
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    'material_id', (
-        "5c66e1ca-eb52-47e5-af50-c48b345c7e6c",
-        None,
-    )
-)
-async def test_get_notes(material_id: str | None):
-    print(material_id)
-    notes = await db.get_notes(material_id=material_id)
-
-    assert all(not note.is_deleted for note in notes), "Deleted note found"
-    if material_id:
-        assert all(note.material_id == material_id for note in notes), "Note with wrong material id found"
-
-    stmt = sa.select(sa.func.count(1))\
-        .select_from(models.Notes)\
-        .where(~models.Notes.c.is_deleted)
-
-    if material_id:
-        stmt = stmt.where(models.Notes.c.material_id == material_id)
-
-    async with database.session() as ses:
-        notes_count = await ses.scalar(stmt)
-
-    assert len(notes) == notes_count, f"Some notes missed, {len(notes)} != {len(notes_count)}"
-
-
-@pytest.mark.asyncio
-async def test_get_all_notes_count():
-    notes = await db.get_notes()
-    notes.sort(key=lambda note: note.material_id)
-
-    notes_count = {
-        material_id: len(list(group))
-        for material_id, group in groupby(notes, key=lambda note: note.material_id)
-    }
-
-    test_result = await db.get_all_notes_count()
-
-    assert notes_count == test_result
-
-
-@pytest.mark.asyncio
 async def test_get_material_types():
     types = await db.get_material_types()
     type_values = set(types.values())
@@ -88,6 +45,49 @@ async def test_get_material_with_notes_titles():
 
     assert len(titles) == len(expected)
     assert set(titles.keys()) == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    'material_id', (
+        "5c66e1ca-eb52-47e5-af50-c48b345c7e6c",
+        None,
+    )
+)
+async def test_get_notes(material_id: str | None):
+    print(material_id)
+    notes = await db.get_notes(material_id=material_id)
+
+    assert all(not note.is_deleted for note in notes), "Deleted note found"
+    if material_id:
+        assert all(note.material_id == material_id for note in notes), "Note with wrong material id found"
+
+    stmt = sa.select(sa.func.count(1)) \
+        .select_from(models.Notes) \
+        .where(~models.Notes.c.is_deleted)
+
+    if material_id:
+        stmt = stmt.where(models.Notes.c.material_id == material_id)
+
+    async with database.session() as ses:
+        notes_count = await ses.scalar(stmt)
+
+    assert len(notes) == notes_count, f"Some notes missed, {len(notes)} != {len(notes_count)}"
+
+
+@pytest.mark.asyncio
+async def test_get_all_notes_count():
+    notes = await db.get_notes()
+    notes.sort(key=lambda note: note.material_id)
+
+    notes_count = {
+        material_id: len(list(group))
+        for material_id, group in groupby(notes, key=lambda note: note.material_id)
+    }
+
+    test_result = await db.get_all_notes_count()
+
+    assert notes_count == test_result
 
 
 @pytest.mark.asyncio
