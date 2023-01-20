294b0501f93153312aaf0e0d155a56c70a6b8bc7
kunansy <kolobov.kirill@list.ru>
2023-01-20 15:49:01 +0300

    test_get_last_date() added

--- tracker/tests/test_statistics.py
+++ tracker/tests/test_statistics.py
@@ -14,3 +14,11 @@ async def test_get_start_date():
     start_date = await st._get_start_date()
 
     assert min(records, key=lambda record: record.date).date == start_date
+
+
+@pytest.mark.asyncio
+async def test_get_last_date():
+    records = await db.get_log_records()
+    last_date = await st._get_last_date()
+
+    assert max(records, key=lambda record: record.date).date == last_date
