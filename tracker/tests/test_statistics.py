4153778c7e26dd3a786bb82effb4763b53b6432f
kunansy <kolobov.kirill@list.ru>
2023-01-20 15:48:00 +0300

    test_get_start_date() added

---
+++ tracker/tests/test_statistics.py
@@ -0,0 +1,16 @@
+import pytest
+
+from tracker.reading_log import statistics as st, db
+
+
+@pytest.mark.asyncio
+async def test_get_m_log_statistics():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_get_start_date():
+    records = await db.get_log_records()
+    start_date = await st._get_start_date()
+
+    assert min(records, key=lambda record: record.date).date == start_date
