31be9d57766da984ebe7d5c946fea3f5fcac1404
kunansy <kolobov.kirill@list.ru>
2023-01-21 08:22:56 +0300

    test_session_error() added

--- tracker/tests/test_database.py
+++ tracker/tests/test_database.py
@@ -10,3 +10,14 @@ async def test_session():
 
     async with database.session() as ses:
         assert await ses.scalar(stmt) == 2
+
+
+@pytest.mark.asyncio
+async def test_session_error():
+    stmt = sa.text('SELECT 1 / 0')
+
+    with pytest.raises(database.DatabaseException) as e:
+        async with database.session() as ses:
+            await ses.scalar(stmt)
+
+    assert "division by zero" in str(e.value)
