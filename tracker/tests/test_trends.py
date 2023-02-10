357550dcf26c9d585dc5d502862896aa517490a4
kunansy <kolobov.kirill@list.ru>
2023-02-10 11:00:31 +0300

    test_span_methods() added

--- tracker/tests/test_trends.py
+++ tracker/tests/test_trends.py
@@ -2,6 +2,7 @@ import datetime
 
 import pytest
 
+from tracker.common import settings
 from tracker.system import trends
 
 
@@ -16,3 +17,18 @@ def test_get_span(size):
 
     assert span.stop == datetime.date.today()
     assert span.start == span.stop - datetime.timedelta(days=size - 1)
+
+
+@pytest.mark.parametrize(
+    'size', (
+        1, 0, 6, 7, 34
+    )
+)
+def test_span_methods(size):
+    span = trends._get_span(size)
+
+    today = datetime.date.today()
+    start = today - datetime.timedelta(days=size - 1)
+
+    assert span.format() == f"{start.strftime(settings.DATE_FORMAT)}_{today.strftime(settings.DATE_FORMAT)}"
+    assert str(span) == f"[{start.strftime(settings.DATE_FORMAT)}; {today.strftime(settings.DATE_FORMAT)}]"
