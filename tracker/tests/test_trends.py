89fe4941ef7e23a0dd89e2c4c7f2ecdef3ed1fd6
kunansy <kolobov.kirill@list.ru>
2023-02-10 11:24:25 +0300

    testing zero spans removed

--- tracker/tests/test_trends.py
+++ tracker/tests/test_trends.py
@@ -8,7 +8,7 @@ from tracker.system import trends
 
 @pytest.mark.parametrize(
     'size', (
-        1, 0, 6, 7, 34
+        1, 6, 7, 34
     )
 )
 def test_get_span(size):
@@ -21,7 +21,7 @@ def test_get_span(size):
 
 @pytest.mark.parametrize(
     'size', (
-        1, 0, 6, 7, 34
+        1, 6, 7, 34
     )
 )
 def test_span_methods(size):
