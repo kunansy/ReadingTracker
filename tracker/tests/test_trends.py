fe1365eb284550d87a3655cf98a0745321e74b21
kunansy <kolobov.kirill@list.ru>
2023-02-10 11:24:03 +0300

    test_iterate_over_span() added

--- tracker/tests/test_trends.py
+++ tracker/tests/test_trends.py
@@ -32,3 +32,21 @@ def test_span_methods(size):
 
     assert span.format() == f"{start.strftime(settings.DATE_FORMAT)}_{today.strftime(settings.DATE_FORMAT)}"
     assert str(span) == f"[{start.strftime(settings.DATE_FORMAT)}; {today.strftime(settings.DATE_FORMAT)}]"
+
+
+@pytest.mark.parametrize(
+    'start,size', (
+        (datetime.date(2023, 1, 1), 10),
+        (datetime.date(2023, 1, 1), 5),
+        (datetime.date(2023, 1, 1), 1),
+    )
+)
+def test_iterate_over_span(start, size):
+    stop = start + datetime.timedelta(days=size - 1)
+    span = trends.TimeSpan(start=start, stop=stop, span_size=size)
+
+    result = [date for date in trends._iterate_over_span(span, size=size)]
+
+    assert len(result) == size
+    for index, day in enumerate(range(size)):
+        assert result[index] == start + datetime.timedelta(days=day)
