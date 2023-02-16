6337e7e8ea22a414666b5c52bcad3beb11116284
kunansy <kolobov.kirill@list.ru>
2023-02-16 12:05:12 +0300

    context menu for materials added

--- static/js/common.js
+++ static/js/common.js
@@ -37,3 +37,100 @@ document.addEventListener("DOMContentLoaded", () => {
         textInput.addEventListener('keyup', onClick, true);
     }
 })
+
+const contextMenu = document.getElementById("context-menu");
+const scope = document.querySelector("body");
+
+const normalizePosition = (mouseX, mouseY) => {
+    // ? compute what is the mouse position relative to the container element (scope)
+    let {
+      left: scopeOffsetX,
+      top: scopeOffsetY,
+    } = scope.getBoundingClientRect();
+
+    scopeOffsetX = scopeOffsetX < 0 ? 0 : scopeOffsetX;
+    scopeOffsetY = scopeOffsetY < 0 ? 0 : scopeOffsetY;
+
+    const scopeX = mouseX - scopeOffsetX;
+    const scopeY = mouseY - scopeOffsetY;
+
+    // ? check if the element will go out of bounds
+    const outOfBoundsOnX = scopeX + contextMenu.clientWidth > scope.clientWidth;
+    const outOfBoundsOnY = scopeY + contextMenu.clientHeight > scope.clientHeight;
+
+    let normalizedX = mouseX;
+    let normalizedY = mouseY;
+
+    // ? normalize on X
+    if (outOfBoundsOnX) {
+        normalizedX = scopeOffsetX + scope.clientWidth - contextMenu.clientWidth;
+    }
+    // ? normalize on Y
+    if (outOfBoundsOnY) {
+        normalizedY = scopeOffsetY + scope.clientHeight - contextMenu.clientHeight;
+    }
+
+    return { normalizedX, normalizedY };
+};
+
+const addContextMenu = (selector, addContextMenuItemsFn) => {
+    scope.querySelectorAll(selector).forEach((item) => {
+        item.addEventListener("contextmenu", (event) => {
+            if (item.contains(event.target)) {
+                addContextMenuItemsFn(item);
+
+                event.preventDefault();
+
+                const { clientX: mouseX, clientY: mouseY } = event;
+
+                const { normalizedX, normalizedY } = normalizePosition(mouseX, mouseY);
+
+                contextMenu.classList.remove("visible");
+
+                contextMenu.style.top = `${normalizedY}px`;
+                contextMenu.style.left = `${normalizedX}px`;
+
+                setTimeout(() => {
+                    contextMenu.classList.add("visible");
+                });
+            }
+        });
+    });
+}
+
+scope.addEventListener("click", (e) => {
+    // ? close the menu if the user clicks outside of it
+    if (e.target.offsetParent !== contextMenu) {
+      contextMenu.classList.remove("visible");
+      contextMenu.innerHTML = '';
+    }
+});
+
+const editMaterialBtn = (material_id) => {
+    const node = document.createElement("div");
+    node.className = "item";
+    node.onclick = () => {window.open('/materials/update-view/?material_id=' + material_id)};
+    node.textContent = "Edit";
+
+    return node;
+}
+
+const openMaterialNotesBtn = (material_id) => {
+    const node = document.createElement("div");
+    node.className = "item";
+    node.onclick = () => {window.open('/notes?material_id=' + material_id)};
+    node.textContent = "Open notes";
+
+    return node;
+}
+
+const addMaterialContextMenuItems = (material) => {
+    // ? on duplicate click don't add items again;
+    if (contextMenu.children.length > 0) {
+        return;
+    }
+    contextMenu.appendChild(editMaterialBtn(material.id));
+    contextMenu.appendChild(openMaterialNotesBtn(material.id));
+}
+
+addContextMenu('.material', addMaterialContextMenuItems);
