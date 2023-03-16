f434e52f2e736ca7ca757b4c10da9529f74329d2
kunansy <kolobov.kirill@list.ru>
2023-03-16 08:37:59 +0300

    test_get_means() updated

--- tracker/tests/test_materials.py
+++ tracker/tests/test_materials.py
@@ -34,10 +34,10 @@ async def get_statuses() -> list[db.Status]:
 
 
 @pytest.mark.asyncio
-async def test_get_mean_read_pages():
-    from tracker.reading_log.statistics import get_mean_read_pages
+async def test_get_means():
+    from tracker.reading_log.statistics import get_means
 
-    assert await db.get_mean_read_pages() == await get_mean_read_pages()
+    assert await db.get_means() == await get_means()
 
 
 @pytest.mark.asyncio
