6e76b5ab9f84b753b09bd8cc96aebf80f0ab5690
kunansy <kolobov.kirill@list.ru>
2023-11-26 13:10:55 +0300

    remove google drive logic

--- tracker/google_drive/drive_api.py
+++ tracker/google_drive/drive_api.py
@@ -1,14 +1,8 @@
-import contextlib
-import os
-import tempfile
 import time
-from functools import lru_cache
 from pathlib import Path
-from typing import Any, AsyncGenerator
+from typing import Any
 
-import aiogoogle
 import orjson
-from aiogoogle.auth.creds import ServiceAccountCreds
 from grpc.aio import insecure_channel as grpc_chan
 
 from tracker.common import settings, database
@@ -17,79 +11,8 @@ from tracker.google_drive import db
 from tracker.protos import backup_pb2_grpc, backup_pb2
 
 
-SCOPES = ["https://www.googleapis.com/auth/drive"]
-
-
 class GoogleDriveException(Exception):
     pass
-
-
-@lru_cache
-def _get_drive_creds() -> ServiceAccountCreds:
-    creds = ServiceAccountCreds(scopes=SCOPES, **settings.DRIVE_CREDS)
-
-    return creds
-
-
-@contextlib.asynccontextmanager
-async def _drive_client() -> AsyncGenerator[
-    tuple[aiogoogle.Aiogoogle, aiogoogle.GoogleAPI], None
-]:
-    creds = _get_drive_creds()
-
-    async with aiogoogle.Aiogoogle(service_account_creds=creds) as client:
-        drive_v3 = await client.discover("drive", "v3")
-        try:
-            yield client, drive_v3
-        except Exception as e:
-            logger.exception("Error with the client, %s", repr(e))
-            raise GoogleDriveException(e) from e
-
-
-async def _get_folder_id(*, folder_name: str = "tracker") -> str:
-    async with _drive_client() as (client, drive):
-        response = await client.as_service_account(
-            drive.files.list(
-                q=f"name = '{folder_name}'", spaces="drive", fields="files(id)"
-            )
-        )
-
-    return response["files"][0]["id"]
-
-
-async def _get_last_dump_id() -> str:
-    logger.debug("Getting last dump started")
-    folder_id = await _get_folder_id()
-    query = f"name contains 'tracker_' and '{folder_id}' in parents"
-
-    async with _drive_client() as (client, drive):
-        response = await client.as_service_account(
-            drive.files.list(
-                q=query, spaces="drive", fields="files(id,modifiedTime,name)"
-            )
-        )
-    files = response["files"]
-    files.sort(key=lambda resp: resp["modifiedTime"], reverse=True)
-
-    logger.debug("%s files found", len(files))
-    return files[0]["id"]
-
-
-async def _get_file_content(file_id: str) -> dict[str, Any]:
-    logger.debug("Getting file id='%s'", file_id)
-
-    tmp_file = tempfile.NamedTemporaryFile(delete=False)
-
-    async with _drive_client() as (client, drive):
-        await client.as_service_account(
-            drive.files.get(fileId=file_id, download_file=tmp_file.name, alt="media"),
-        )
-
-    file = orjson.loads(tmp_file.read())
-
-    tmp_file.close()
-    os.remove(tmp_file.name)  # noqa: PL107
-    return file
 
 
 def _read_local_dump(filepath: Path) -> dict[str, Any]:
