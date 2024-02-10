deb6ad39f15d9a931c2ac17a03f8d2508e003b50
kunansy <kolobov.kirill@list.ru>
2024-02-10 16:32:31 +0300

    implement parser for youtube lectures

---
+++ ytParser.js
@@ -0,0 +1,27 @@
+const parseDuration = (duration) => {
+    let parts = duration.split(":");
+    let total = 0;
+
+    switch (parts.length) {
+        case 3:
+            total += parseInt(parts[0]) * 60;
+        case 2:
+            total += parseInt(parts[1]);
+        case 1:
+            total += Math.floor(parseInt(parts[0]) / 60);
+    }
+
+    return total;
+}
+
+const parse = () => {
+    let title = document.querySelector("div#title.ytd-watch-metadata yt-formatted-string").textContent;
+    let author = document.querySelector("div#owner.ytd-watch-metadata yt-formatted-string").textContent;
+    let duration = document.querySelector("div.ytp-time-display span.ytp-time-duration").textContent;
+    
+    return {
+        title: title,
+        author: author,
+        duration: parseDuration(duration)
+    }
+}
