252f44995bfc27c42215e6a63b049ccfa3005886
kunansy <kolobov.kirill@list.ru>
2024-03-08 20:16:41 +0300

    create producer in a closure

--- tracker/common/kafka.py
+++ tracker/common/kafka.py
@@ -5,26 +5,36 @@ import aiokafka
 from tracker.common import logger, settings
 
 
-_PRODUCER = aiokafka.AIOKafkaProducer(
-    bootstrap_servers=settings.KAFKA_URL,
-    enable_idempotence=True,
-)
+async def _producer() -> aiokafka.AIOKafkaProducer:
+    producer: aiokafka.AIOKafkaProducer | None = None
+
+    async def wrapper() -> aiokafka.AIOKafkaProducer:
+        nonlocal producer
+        if not producer:
+            producer = aiokafka.AIOKafkaProducer(
+                bootstrap_servers=settings.KAFKA_URL,
+                enable_idempotence=True,
+            )
+        return producer
+
+    return await wrapper()
 
 
 async def _send_one(key: str, value: str, *, topic: str) -> None:
-    await _PRODUCER.start()
+    producer = await _producer()
+    await producer.start()
 
     try:
         logger.logger.debug(
             "Sending a message: key=%s, value=%s, to %s", key, value, topic,
         )
-        await _PRODUCER.send_and_wait(topic, key=key.encode(), value=value.encode())
+        await producer.send_and_wait(topic, key=key.encode(), value=value.encode())
     except aiokafka.errors.KafkaError:
         logger.logger.exception("Could not send message")
     else:
         logger.logger.debug("Message sent")
     finally:
-        await _PRODUCER.stop()
+        await producer.stop()
 
 
 async def repeat_note(note_id: uuid.UUID | str) -> None:
