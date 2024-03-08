732a6ece906e2a48939a08f4158efd87573535fd
kunansy <kolobov.kirill@list.ru>
2024-03-08 18:50:15 +0300

    make producer a global var

--- tracker/common/kafka.py
+++ tracker/common/kafka.py
@@ -6,22 +6,24 @@ import aiokafka
 from tracker.common import logger, settings
 
 
+_PRODUCER = aiokafka.AIOKafkaProducer(
+    bootstrap_servers=settings.KAFKA_URL,
+    enable_idempotence=True,
+)
+
+
 async def _send_one(key: str, value: str, *, topic: str) -> None:
-    producer = aiokafka.AIOKafkaProducer(
-        bootstrap_servers=settings.KAFKA_URL,
-        enable_idempotence=True,
-    )
-    await producer.start()
+    await _PRODUCER.start()
 
     try:
         logger.logger.debug("Sending a message: key=%s, value=%s, to %s", key, value, topic)
-        await producer.send_and_wait(topic, key=key.encode(), value=value.encode())
+        await _PRODUCER.send_and_wait(topic, key=key.encode(), value=value.encode())
     except aiokafka.errors.KafkaError:
         logger.logger.exception("Could not send message")
     else:
         logger.logger.debug("Message sent")
     finally:
-        await producer.stop()
+        await _PRODUCER.stop()
 
 
 async def repeat_note(note_id: uuid.UUID | str) -> None:
