9946907265db9cbf45ddec43da446aaba2d1cdbe
kunansy <kolobov.kirill@list.ru>
2024-03-08 18:47:09 +0300

    start kafka api

---
+++ tracker/common/kafka.py
@@ -0,0 +1,28 @@
+import asyncio
+import uuid
+
+import aiokafka
+
+from tracker.common import logger, settings
+
+
+async def _send_one(key: str, value: str, *, topic: str) -> None:
+    producer = aiokafka.AIOKafkaProducer(
+        bootstrap_servers=settings.KAFKA_URL,
+        enable_idempotence=True,
+    )
+    await producer.start()
+
+    try:
+        logger.logger.debug("Sending a message: key=%s, value=%s, to %s", key, value, topic)
+        await producer.send_and_wait(topic, key=key.encode(), value=value.encode())
+    except aiokafka.errors.KafkaError:
+        logger.logger.exception("Could not send message")
+    else:
+        logger.logger.debug("Message sent")
+    finally:
+        await producer.stop()
+
+
+async def repeat_note(note_id: uuid.UUID | str) -> None:
+    await _send_one("note_id", note_id, topic=settings.KAFKA_REPEAT_NOTES_TOPIC)
