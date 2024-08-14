fbd0b638dc8f27cfb5c848ee7c81895d3023871e
kunansy <kolobov.kirill@list.ru>
2024-08-14 13:25:18 +0300

    add Card schema

--- tracker/cards/schemas.py
+++ tracker/cards/schemas.py
@@ -9,3 +9,10 @@ class GetHasCards(CustomBaseModel):
     note_id: UUID
     has_cards: bool
     cards_count: NonNegativeInt
+
+
+class Card(CustomBaseModel):
+    material_id: UUID
+    note_id: UUID
+    question: str
+    answer: str | None
