99866fef48f215a76ea561248f1c1818ec5c82a1
kunansy <kolobov.kirill@list.ru>
2024-08-18 10:34:19 +0300

    get card fields from http form

--- tracker/cards/schemas.py
+++ tracker/cards/schemas.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from fastapi import Form
 from pydantic import NonNegativeInt
 
 from tracker.common.schemas import CustomBaseModel
@@ -16,3 +17,17 @@ class Card(CustomBaseModel):
     note_id: UUID
     question: str
     answer: str | None
+
+    def __init__(
+        self,
+        material_id: UUID = Form(...),
+        note_id: UUID = Form(...),
+        question: str = Form(...),
+        answer: str | None = Form(None),
+    ):
+        super().__init__(
+            material_id=material_id,
+            note_id=note_id,
+            question=question,
+            answer=answer,
+        )
