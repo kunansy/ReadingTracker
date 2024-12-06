d8d39515114806f5d65c6547e9640ec0cd23a756
kunansy <kolobov.kirill@list.ru>
2024-12-06 22:22:23 +0300

    implement mock_trans

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import asyncio
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
 import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from tracker.common import database
 
 
 @pytest.fixture(scope="session")
@@ -9,3 +14,15 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@asynccontextmanager
+async def mock_trans(**kwargs) -> AsyncGenerator[AsyncSession, None]:
+    new_session = AsyncSession(**kwargs, bind=database.engine)
+
+    try:
+        async with new_session.begin() as t:
+            yield new_session
+            await t.rollback()
+    finally:
+        await new_session.close()
