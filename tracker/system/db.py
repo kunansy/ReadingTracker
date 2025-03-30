d9256d05912651ea7e3f351d5476187f2a42ad93
kunansy <kolobov.kirill@list.ru>
2025-03-30 19:23:09 +0300

    format ruff

--- tracker/system/db.py
+++ tracker/system/db.py
@@ -97,9 +97,9 @@ async def create_reading_graphic(*, material_id: UUID, last_days: int) -> str:
     return base64.b64encode(buff.getvalue()).decode("utf-8")
 
 
-async def _calculate_outline_percentage() -> (
-    dict[enums.MaterialTypesEnum, dict[bool, int]]
-):
+async def _calculate_outline_percentage() -> dict[
+    enums.MaterialTypesEnum, dict[bool, int],
+]:
     stmt = sa.select(
         models.Materials.c.material_type,
         models.Materials.c.is_outlined,
