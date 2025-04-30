271b8218db8c16660d731426bb0f550e65e23fb8
kunansy <kolobov.kirill@list.ru>
2025-04-30 22:28:23 +0300

    simplify Card schema

--- tracker/cards/schemas.py
+++ tracker/cards/schemas.py
@@ -24,17 +24,3 @@ class Card(CustomBaseModel):
     note_id: UUID
     question: str
     answer: str | None
-
-    def __init__(
-        self,
-        material_id: UUID = Form(...),
-        note_id: UUID = Form(...),
-        question: str = Form(...),
-        answer: str | None = Form(None),
-    ):
-        super().__init__(
-            material_id=material_id,
-            note_id=note_id,
-            question=question,
-            answer=answer,
-        )
