559dbbc2a3c739db87d9b2a764f2af49fd3013f7
kunansy <kolobov.kirill@list.ru>
2025-04-30 22:25:11 +0300

    implement DEFAULT_UUID

--- tracker/common/schemas.py
+++ tracker/common/schemas.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Annotated, Literal
+from uuid import UUID
+
+from pydantic import BaseModel, BeforeValidator, ConfigDict
 
 
 class CustomBaseModel(BaseModel):
     model_config = ConfigDict(extra="forbid", str_strip_whitespace=True)
+
+
+def str_to_uuid(v: Literal[""] | UUID | None) -> UUID | None:
+    if not v:
+        return None
+    return v
+
+
+DEFAULT_UUID = Annotated[UUID | None, BeforeValidator(str_to_uuid)]
+
