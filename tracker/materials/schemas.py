d85148c0952fe3a7a6787d85f805f6fcdf187816
kunansy <kolobov.kirill@list.ru>
2025-04-30 22:30:11 +0300

    simplify UpdateMaterial schema

--- tracker/materials/schemas.py
+++ tracker/materials/schemas.py
@@ -1,7 +1,6 @@
 from typing import Literal
 from uuid import UUID
 
-from fastapi import Form
 from pydantic import HttpUrl, conint
 
 from tracker.common.schemas import CustomBaseModel
@@ -24,26 +23,6 @@ class Material(CustomBaseModel):
 
 class UpdateMaterial(Material):
     material_id: UUID
-
-    def __init__(
-        self,
-        material_id: UUID = Form(...),
-        title: str = Form(...),
-        authors: str = Form(...),
-        pages: int = Form(...),
-        material_type: enums.MaterialTypesEnum = Form(...),
-        tags: str | None = Form(None),
-        link: HttpUrl | None = Form(None),
-    ) -> None:
-        super().__init__(
-            material_id=material_id,
-            title=title,
-            authors=authors,
-            pages=pages,
-            material_type=material_type,
-            tags=tags,
-            link=link,
-        )
 
 
 class ParsedMaterial(CustomBaseModel):
