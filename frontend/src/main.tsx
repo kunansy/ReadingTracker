dbefabf9fecba7c7b07431b5af91917eee84632c
kunansy <kolobov.kirill@list.ru>
2026-05-02 18:28:01 +0300

    refactor styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 
 import "../../static/style.css";
-import "../../static/materials/queue.css";
 import "../../static/materials/reading.css";
 import "../../static/materials/completed.css";
 import "../../static/materials/repeating_queue.css";
 import "../../static/materials/add_material.css";
 import "../../static/notes/notes.css";
 import "../../static/notes/add_note.css";
-import "../../static/reading_log/reading_log.css";
 import "../../static/reading_log/add_log_record.css";
 import "../../static/cards/cards_list.css";
 import "../../static/cards/add_card.css";
