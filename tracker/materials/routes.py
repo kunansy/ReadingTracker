080fe55f17fb8beb061f037ebde06166b664a9a0
kunansy <kolobov.kirill@list.ru>
2026-05-09 11:24:22 +0300

    remove material html routes

--- tracker/materials/routes.py
+++ tracker/materials/routes.py
@@ -3,9 +3,7 @@
 from fastapi import APIRouter
 
 from tracker.materials.action_routes import router as action_router
-from tracker.materials.html_routes import router as html_router
 
 
 router = APIRouter()
-router.include_router(html_router)
 router.include_router(action_router)
